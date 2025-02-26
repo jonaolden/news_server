be797d5daa032e001878e81f1d38935be62ebca8
jonaolden <oldenburg.jonatan@gmail.com>
2025-02-26 19:20:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,21 @@ export CALIBRE_PASSWORD="${CALIBRE_PASSWORD:-admin}"
 echo "Using library folder: $LIBRARY_FOLDER"
 echo "Using recipes folder: $RECIPES_FOLDER"
 
+# Fix permissions on mounted volumes
+# This ensures calibre user can write to these directories
+chown -R calibre:calibre "$LIBRARY_FOLDER" "$RECIPES_FOLDER" "$USER_DB"
+chmod -R 755 "$LIBRARY_FOLDER" "$RECIPES_FOLDER"
+
 # 1. Setup cron to use the environment variables
 bash /opt/setup_cron.sh
 
 # 2. Run an immediate news download to populate library
-bash /opt/download_news.sh
+# Execute as calibre user
+su - calibre -c "bash /opt/download_news.sh"
 
-# 3. Start calibre-server
-calibre-server "$LIBRARY_FOLDER" \
+# 3. Start calibre-server as calibre user
+su - calibre -c "calibre-server '$LIBRARY_FOLDER' \
   --enable-auth \
-  --userdb "$USER_DB" \
-  --username="$CALIBRE_USER" \
-  --password="$CALIBRE_PASSWORD"
+  --userdb '$USER_DB' \
+  --username='$CALIBRE_USER' \
+  --password='$CALIBRE_PASSWORD'"
